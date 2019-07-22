15fafaf3fb22adcbcb8f06676e01ba87f9caa3f7
ifonl <ifonl@DESKTOP-666RA1I>
2019-07-22 17:06:33 +0900

    190722 Algorithm Study 02

--- AlgorithmStudy/src/AStudy/BJ_2675.java
+++ AlgorithmStudy/src/AStudy/BJ_2675.java
@@ -7,12 +7,12 @@ public class BJ_2675 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		//문자열 S 입력
 		int num = sc.nextInt();
 		for (int i = 0; i < num; i++) {
 			int count = sc.nextInt();
 			String str = sc.nextLine();
-
+			//문자를 R번 반복 후 새 문자열 P 만들기
 			String result = "";
 			for (int j = 0; j < str.length(); j++) {
 				for (int k = 0; k < count; k++)
@@ -20,6 +20,8 @@ public class BJ_2675 {
 			}
 			result = result.replaceAll(" ", "");//replaceAll : 첫번째 인자값은 변환하고자 하는 대상
 												//두 번째 인자값은 변환할 문자 값
+			
+			//출력
 			System.out.println(result);
 		}
 
